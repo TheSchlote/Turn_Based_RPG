45a9ad283bab10304c70986798e1546d76a030a2
TheSchlote <TheSchlote@gmail.com>
2020-12-30 22:21:08 -0600

    First commit
    Brining over Bellas Castle Stuff and will trim out what i dont need/want

---
+++ TurnBasedRPG/Assets/Scripts/Battle/BattleMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMachine : MonoBehaviour
+{
+    //Units Spawn in
+
+    //Read Teams
+    Dictionary<int, Unit> allUnits = new Dictionary<int, Unit>();
+    Dictionary<int, Unit> heroUnits = new Dictionary<int, Unit>();
+    Dictionary<int, Unit> enemyUnits = new Dictionary<int, Unit>();
+
+    Dictionary<int, Unit> turnOrder = new Dictionary<int, Unit>();
+
+}
---
+++ TurnBasedRPG/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Unit : ScriptableObject
+{
+    public string unitName;
+    //different colors mayber
+
+    public int maxHP;
+    public int curHP;
+    public int maxMP;
+    public int curMP;
+
+    public int Attack;
+    public int Defense;
+    public int Speed;
+}
